a2ef563caaaf189dd255a1925668f3816872af10
Kaic Furushima <kaic.fbs@gmail.com>
2024-10-06 18:08:04 -0300

    Exercício 3 adicionado

---
+++ Desafio3-TJB-DIO/iPhone.java
@@ -0,0 +1,32 @@
+public class iPhone extends AparelhoTelefonico implements ReprodutorMusical, NavegadorInternet{
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+}
