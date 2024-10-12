b89be01f970b4484ebf7f2965f02e79a201b488f
Kaic Furushima <kaic.fbs@gmail.com>
2024-10-12 01:49:05 -0300

    Alterado tag Component para Service no Desafio 6

--- Desafio6-TJB-DIO/src/main/java/one/digitalinnovation/Desafio_6/service/ViaCepService.java
+++ Desafio6-TJB-DIO/src/main/java/one/digitalinnovation/Desafio_6/service/ViaCepService.java
@@ -2,11 +2,11 @@ package one.digitalinnovation.Desafio_6.service;
 
 import one.digitalinnovation.Desafio_6.model.Endereco;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Component
+@Service
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
